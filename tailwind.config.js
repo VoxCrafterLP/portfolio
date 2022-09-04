990315020fa532e3c4dafbdc1dc7bb529e43f36e
VoxCrafterLP <voxcrafterlp@gmail.com>
2022-09-04 23:59:45 +0200

    Set up tailwind and prepare folder structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
+    fontFamily: {
+      sans: '"Roboto"',
+      display: ['"Roboto"', '"sans-serif"'],
+      body: ['"Roboto"', '"sans-serif"'],
+    },
+    screens: {
+      'sm': '950px',
+      'md': '1000px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {}
   },
   plugins: []
