4b584db30a41b638b2531fc811895e98e51b187d
VoxCrafterLP <voxcrafterlp@gmail.com>
2022-09-08 21:37:35 +0200

    Add projects title

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -20,48 +20,26 @@ const Projects = () => {
 	};
 
 	return (
-		<motion.div
-			className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-12 p-12"
-			variants={wrapperAnimation}
-			initial="hidden"
-			animate="visible"
-		>
-			<ProjectCard
-				name="Uelzechtkanal"
-				description="Tempor delectus interesset quaerendum utinam constituam latine voluptaria saepe dui. Simul eum dapibus populo leo. Habemus vis facilisi lorem nostrum definiebas assueverit luctus duis."
-				type="Fullstack web application"
-				details="Posidonium eleifend quam sententiae vulputate maximus donec. Parturient iuvaret senectus viverra rhoncus reprehendunt alienum reque atomorum. Inimicus menandri arcu tale a altera nonumy neglegentur. Malorum inani curae prodesset honestatis."
-				link="https://uelzechtkanal.lu/"
-			/>
-			<ProjectCard
-				name="Uelzechtkanal"
-				description="Tempor delectus interesset quaerendum utinam constituam latine voluptaria saepe dui. Simul eum dapibus populo leo. Habemus vis facilisi lorem nostrum definiebas assueverit luctus duis."
-				type="Fullstack web application"
-				details="Posidonium eleifend quam sententiae vulputate maximus donec. Parturient iuvaret senectus viverra rhoncus reprehendunt alienum reque atomorum. Inimicus menandri arcu tale a altera nonumy neglegentur. Malorum inani curae prodesset honestatis."
-				link="https://uelzechtkanal.lu/"
-			/>
-			<ProjectCard
-				name="Uelzechtkanal"
-				description="Tempor delectus interesset quaerendum utinam constituam latine voluptaria saepe dui. Simul eum dapibus populo leo. Habemus vis facilisi lorem nostrum definiebas assueverit luctus duis."
-				type="Fullstack web application"
-				details="Posidonium eleifend quam sententiae vulputate maximus donec. Parturient iuvaret senectus viverra rhoncus reprehendunt alienum reque atomorum. Inimicus menandri arcu tale a altera nonumy neglegentur. Malorum inani curae prodesset honestatis."
-				link="https://uelzechtkanal.lu/"
-			/>
-			<ProjectCard
-				name="Uelzechtkanal"
-				description="Tempor delectus interesset quaerendum utinam constituam latine voluptaria saepe dui. Simul eum dapibus populo leo. Habemus vis facilisi lorem nostrum definiebas assueverit luctus duis."
-				type="Fullstack web application"
-				details="Posidonium eleifend quam sententiae vulputate maximus donec. Parturient iuvaret senectus viverra rhoncus reprehendunt alienum reque atomorum. Inimicus menandri arcu tale a altera nonumy neglegentur. Malorum inani curae prodesset honestatis."
-				link="https://uelzechtkanal.lu/"
-			/>
-			<ProjectCard
-				name="Uelzechtkanal"
-				description="Tempor delectus interesset quaerendum utinam constituam latine voluptaria saepe dui. Simul eum dapibus populo leo. Habemus vis facilisi lorem nostrum definiebas assueverit luctus duis."
-				type="Fullstack web application"
-				details="Posidonium eleifend quam sententiae vulputate maximus donec. Parturient iuvaret senectus viverra rhoncus reprehendunt alienum reque atomorum. Inimicus menandri arcu tale a altera nonumy neglegentur. Malorum inani curae prodesset honestatis."
-				link="https://uelzechtkanal.lu/"
-			/>
-		</motion.div>
+		<>
+			<div className="flex flex-col items-center py-16">
+				<h1 className="text-4xl text-white font-semibold mb-4">My projects</h1>
+				<div className="h-1 w-12 bg-purple-800 rounded-lg"/>
+			</div>
+			<motion.div
+				className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-12 p-12"
+				variants={wrapperAnimation}
+				initial="hidden"
+				animate="visible"
+			>
+				<ProjectCard
+					name="Uelzechtkanal"
+					description="Tempor delectus interesset quaerendum utinam constituam latine voluptaria saepe dui. Simul eum dapibus populo leo. Habemus vis facilisi lorem nostrum definiebas assueverit luctus duis."
+					type="Fullstack web application"
+					details="Posidonium eleifend quam sententiae vulputate maximus donec. Parturient iuvaret senectus viverra rhoncus reprehendunt alienum reque atomorum. Inimicus menandri arcu tale a altera nonumy neglegentur. Malorum inani curae prodesset honestatis."
+					link="https://uelzechtkanal.lu/"
+				/>
+			</motion.div>
+		</>
 	);
 };
 
